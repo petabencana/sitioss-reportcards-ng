9b4cf87a16d17b1de2c7f2a85e901d34c1c5abe9
ruthvik129 <ruthvik129@gmail.com>
2023-11-13 21:14:14 +0530

    add changes to cards for training platform

--- src/app/routes/decks/utils.module.ts
+++ src/app/routes/decks/utils.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { DropdownNotificationComponent } from '../../components/dropdown-notification/dropdown-notification.component';
 import { NavButtonComponent } from '../../components/nav-button/nav-button.component';
 import { TitleBoxComponent } from '../../components/title-box/title-box.component';
+import { ReportTypeButtonComponent } from '../../components/report-type-button/report-type-button.component';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
@@ -14,11 +15,13 @@ import {FormsModule} from '@angular/forms';
     DropdownNotificationComponent,
     NavButtonComponent,
     TitleBoxComponent,
+    ReportTypeButtonComponent
   ],
   exports: [
     DropdownNotificationComponent,
     NavButtonComponent,
     TitleBoxComponent,
+    ReportTypeButtonComponent
   ]
 })
 export class UtilsModule {}
