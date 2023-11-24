e013d797d52b4acd58b0e4c2a776e1cc6e1a96ea
ruthvik129 <ruthvik129@gmail.com>
2023-11-24 11:14:31 +0530

    fix translation issue and training platform flow

--- src/app/routes/decks/volcano/volcano.component.ts
+++ src/app/routes/decks/volcano/volcano.component.ts
@@ -12,6 +12,11 @@ import { environment as env } from '../../../../environments/environment';
   styleUrls: ['./volcano.component.scss']
 })
 export class VolcanoComponent implements OnInit {
+  items: {
+    title: string;
+    subType: 'real' | 'training';
+  }[];
+  showReportTypeButton: boolean = true
 
   constructor(
     public translate: TranslateService,
@@ -37,6 +42,26 @@ export class VolcanoComponent implements OnInit {
     this.deckService.setRoute(route)
   }
 
-  ngOnInit() { }
+  onTypeSelected(type) {
+    this.showReportTypeButton = false;
+    this.deckService.selectReportType(type);
+  }
+
+  get isShowButtons(): boolean {
+    return this.showReportTypeButton;
+  }
+
+  ngOnInit() {
+    this.items = [
+      {
+        title: 'card.type.report.realTypeButton',
+        subType: 'real',
+      },
+      {
+        title: 'card.type.report.trainingTypeButton',
+        subType: 'training',
+      },
+    ];
+  }
 
 }
