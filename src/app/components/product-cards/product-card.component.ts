a333cc3837a1fec9dcaf2858e31e1d3bae7a3084
sanjaypinna <sanjaypinna265@gmail.com>
2024-01-09 15:55:39 +0530

    update cards

--- src/app/components/product-cards/product-card.component.ts
+++ src/app/components/product-cards/product-card.component.ts
@@ -11,11 +11,18 @@ export class CardComponent {
   quantity: number = 0;
 
   cards = [
-    { title: 'Water', description: '5L hot/cold water' },
-    { title: 'Food', description: 'Veg/Nonveg' },
-    { title: 'Food', description: 'Veg/Nonveg' },
-    { title: 'Food', description: 'Veg/Nonveg' },
+    { title: 'Meals', description: 'Veg/Non veg' , category: 'Food'},
+    { title: 'Shirt', description: '1 pair',category: 'Clothes' },
+    { title: 'Dolo', description: '5 Strips',category: 'Med' },
+   
   ];
+
+ categories = [
+    { category: 'Med', description: '' },
+    { category: 'Food', description: '' },
+    { category: 'Clothes', description: '' },
+  ];
+
   constructor( public deckService: DeckService ){}
 
 //   cardsData = [{
@@ -32,13 +39,38 @@ export class CardComponent {
 //   }]
 // ]
   
-  increaseQuantity() {
-    this.quantity++;
-    this.deckService.setWaterQuantiy(this.quantity);
+//   increaseQuantity() {
+//     this.quantity++;
+//     this.deckService.setWaterQuantiy(this.quantity);
+//   }
+//   decreaseQuantity() {
+//     if (this.quantity > 0) {
+//       this.quantity--;
+//     }
+//   }
+// }
+
+
+selectedCategory: string | null = 'Med';
+
+
+get filteredCards() {
+  return this.selectedCategory
+    ? this.cards.filter((card) => card.category === this.selectedCategory)
+    : this.cards;
+}
+
+selectCategory(category: string) {
+  this.selectedCategory = category;
+}
+
+increaseQuantity(card: any) {
+  card.quantity = (card.quantity || 0) + 1;
+}
+
+decreaseQuantity(card: any) {
+  if (card.quantity && card.quantity > 0) {
+    card.quantity -= 1;
   }
-  decreaseQuantity() {
-    if (this.quantity > 0) {
-      this.quantity--;
-    }
-  }
+}
 }
