04dfe68ee58033c4d0fdd92d9abd1bd52d89243d
sanjaypinna <sanjaypinna265@gmail.com>
2024-01-11 19:27:14 +0530

    update product-cards

--- src/app/components/product-cards/product-card.component.ts
+++ src/app/components/product-cards/product-card.component.ts
@@ -1,5 +1,5 @@
 // card.component.ts
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { DeckService } from '../../services/cards/deck.service';
 
 @Component({
@@ -11,66 +11,95 @@ export class CardComponent {
   quantity: number = 0;
 
   cards = [
-    { title: 'Meals', description: 'Veg/Non veg' , category: 'Food'},
-    { title: 'Shirt', description: '1 pair',category: 'Clothes' },
-    { title: 'Dolo', description: '5 Strips',category: 'Med' },
-   
+    {
+      title: 'Meals',
+      description: 'Veg/Non veg',
+      category: 'Food',
+      img: '../../../assets/decks/logistics/products/Water.png',
+    },
+    {
+      title: 'Shirt',
+      description: '1 pair',
+      category: 'Clothes',
+      img: '../../../assets/decks/logistics/products/Blanket.png',
+    },
+    {
+      title: 'Dolo',
+      description: '5 Strips',
+      category: 'Med',
+      img: '../../../assets/decks/logistics/products/Tablet.png',
+    },
   ];
 
- categories = [
-    { category: 'Med', description: '' },
-    { category: 'Food', description: '' },
-    { category: 'Clothes', description: '' },
+  categories = [
+    {
+      category: 'Med',
+      description: '',
+      img: '../../../assets/decks/logistics/products/Firstaid.png',
+    },
+    {
+      category: 'Food',
+      description: '',
+      img: '../../../assets/decks/logistics/products/Food.png',
+    },
+    {
+      category: 'Clothes',
+      description: '',
+      img: '../../../assets/decks/logistics/products/Clothes.png',
+    },
   ];
 
-  constructor( public deckService: DeckService ){}
+  constructor(public deckService: DeckService) {}
 
-//   cardsData = [{
-//     'Food and Water' : [{
-//       name : 'Water',
-//       id : 'water',
+  selectedCategory: string | null = 'Med';
 
-//     },
-  
-//   ]
-//   },
-//   'Clothing' : [{
+  // cart: { title: string; quantity: number,category: string  }[] = [];
 
-//   }]
-// ]
-  
-//   increaseQuantity() {
-//     this.quantity++;
-//     this.deckService.setWaterQuantiy(this.quantity);
-//   }
-//   decreaseQuantity() {
-//     if (this.quantity > 0) {
-//       this.quantity--;
-//     }
-//   }
-// }
-
-
-selectedCategory: string | null = 'Med';
-
-
-get filteredCards() {
-  return this.selectedCategory
-    ? this.cards.filter((card) => card.category === this.selectedCategory)
-    : this.cards;
-}
-
-selectCategory(category: string) {
-  this.selectedCategory = category;
-}
-
-increaseQuantity(card: any) {
-  card.quantity = (card.quantity || 0) + 1;
-}
-
-decreaseQuantity(card: any) {
-  if (card.quantity && card.quantity > 0) {
-    card.quantity -= 1;
+  get filteredCards() {
+    return this.selectedCategory
+      ? this.cards.filter((card) => card.category === this.selectedCategory)
+      : this.cards;
   }
-}
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
+
+  increaseQuantity(card: any) {
+    card.quantity = (card.quantity || 0) + 1;
+    this.recordQuantityChange(card.title, card.quantity, card.category);
+  }
+
+  decreaseQuantity(card: any) {
+    if (card.quantity && card.quantity > 0) {
+      card.quantity -= 1;
+      this.recordQuantityChange(card.title, card.quantity, card.category);
+    }
+  }
+
+  private recordQuantityChange(
+    title: string,
+    quantity: number,
+    category: string
+  ) {
+    // if (quantity === 0) {
+    //   this.cart = this.cart.filter((change) => change.title !== title);
+    // } else {
+    //   const index = this.cart.findIndex((change) => change.title === title);
+
+    //   if (index !== -1) {
+    //     this.cart[index].quantity = quantity;
+    //     this.cart[index].category = category;
+
+    //   } else {
+    //     this.cart.push({ title, quantity , category});
+    //   }
+    // }
+    this.deckService.setSelectedProducts(title, quantity, category);
+  }
+
+  check() {
+    // console.log(this.cart);
+    console.log(this.deckService.selectedProducts);
+  }
 }
