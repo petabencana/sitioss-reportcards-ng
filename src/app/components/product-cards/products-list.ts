f3ed79e4a17765d36ef36fda3d37d8492460e764
sanjaypinna <sanjaypinna265@gmail.com>
2024-01-31 12:00:12 +0530

    add products-list

---
+++ src/app/components/product-cards/products-list.ts
@@ -0,0 +1,266 @@
+export const productsList = [
+  // food
+  {
+    title: 'Water',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/Water.png',
+    quantity: 0,
+  },
+  {
+    title: 'Rice',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/RiceSack.png',
+    quantity: 0,
+  },
+  {
+    title: 'Ready to Eat Meals',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/ReadytoEatMeals.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. E.g. instant porridge, bread, biscuits, nasi bungkus/padang',
+    hasDescription: true,
+  },
+  {
+    title: 'Sembako',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Fresh Produce',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. E.g. (Eggs, Vegetables, Meat, Fruits)',
+    hasDescription: true,
+  },
+  {
+    title: 'Baby Food',
+    description: '',
+    category: 'FOOD/WATER',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+
+  // CLOTHES & SELF PROTECTION KIT
+  {
+    title: 'Women',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder: 'Add description. E.g. Underwear, Bra, Dress',
+    hasDescription: true,
+  },
+  {
+    title: 'Men',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder: 'Add description: E.g. Shirts, Pants, Underwear',
+    hasDescription: true,
+  },
+  {
+    title: 'Footwear',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder: 'Add description: E.g. Sandals, Protective Boots',
+    hasDescription: true,
+  },
+  {
+    title: 'Face Masks',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/Mask.png',
+    quantity: 0,
+  },
+  {
+    title: 'Protective Eye Wear',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Raincoats',
+    description: '',
+    category: 'CLOTHES & SELF PROTECTION KIT',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+
+  // BABIES AND CHILDREN
+
+  {
+    title: 'Clothing',
+    description: '',
+    category: 'BABIES AND CHILDREN',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder: 'Add description. E.g age/size, clothing item',
+    hasDescription: true,
+  },
+  {
+    title: 'Bottles',
+    description: '',
+    category: 'BABIES AND CHILDREN',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Toys',
+    description: '',
+    category: 'BABIES AND CHILDREN',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Pacifier',
+    description: '',
+    category: 'BABIES AND CHILDREN',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+
+  // HOUSEHOLD AND EMERGENCY SUPPLIES
+  {
+    title: 'Utensils',
+    description: '',
+    category: 'HOUSEHOLD AND EMERGENCY SUPPLIES',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder: 'Add description. E.g pan, cutlery',
+    hasDescription: true,
+  },
+  {
+    title: 'Blankets',
+    description: '',
+    category: 'HOUSEHOLD AND EMERGENCY SUPPLIES',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Sleeping Mats',
+    description: '',
+    category: 'HOUSEHOLD AND EMERGENCY SUPPLIES',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Cleaning Supplies',
+    description: '',
+    category: 'HOUSEHOLD AND EMERGENCY SUPPLIES',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. (E.g. Brooms, Detergent, Floor Wiper)',
+    hasDescription: true,
+  },
+
+  {
+    title: 'Flashlight',
+    description: '',
+    category: 'HOUSEHOLD AND EMERGENCY SUPPLIES',
+    img: '../../../assets/decks/logistics/products/Flashlight.png',
+    quantity: 0,
+  },
+
+  // PERSONAL HEALTH
+  {
+    title: 'First aid kit',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/Firstaid.png',
+    quantity: 0,
+  },
+  {
+    title: 'Diarrhea Medication',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Anti Fungal Cream',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/AntiFungalCream.png',
+    quantity: 0,
+  },
+  {
+    title: 'Vitamins & Supplements',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. (E.g. Iron Supplement, Multi vitamin, Vitamin C)',
+    hasDescription: true,
+  },
+  {
+    title: 'Paracetamol',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/Paracetamol.png',
+    quantity: 0,
+  },
+  {
+    title: 'Eucalyptus Oil',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+  },
+  {
+    title: 'Personal Health Equipment',
+    description: '',
+    category: 'PERSONAL HEALTH',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description E.g. Wheelchair, walking stick, oxygen, blood pressure measurement tool, diabetes measurement tool, inhaler',
+    hasDescription: true,
+  },
+  // HYGIENE KITS
+
+  {
+    title: 'Female Hygiene Products',
+    description: '',
+    category: 'HYGIENE KITS',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. E.g. pads, tampons, menstrual cups',
+    hasDescription: true,
+  },
+  {
+    title: 'Oral Care',
+    description: '',
+    category: 'HYGIENE KITS',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. E.g. toothbrush, toothpaste',
+    hasDescription: true,
+  },
+  {
+    title: 'Personal Cleansing',
+    description: '',
+    category: 'HYGIENE KITS',
+    img: '../../../assets/decks/logistics/products/no-pictures.png',
+    quantity: 0,
+    descriptionPlaceholder:
+      'Add description. E.g. soap, shampoo',
+    hasDescription: true,
+  },
+];
