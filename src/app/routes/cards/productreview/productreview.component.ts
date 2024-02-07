bd9cf29ae9701e748825665c3870ff6a6794f468
sanjaypinna <sanjaypinna265@gmail.com>
2024-02-07 18:39:37 +0530

    add captcha verification function

--- src/app/routes/cards/productreview/productreview.component.ts
+++ src/app/routes/cards/productreview/productreview.component.ts
@@ -3,11 +3,10 @@ import { DeckService } from '../../../services/cards/deck.service';
 import { TranslateService } from '@ngx-translate/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-products',
   templateUrl: './productreview.component.html',
-  styleUrls: ['./productreview.component.scss']
+  styleUrls: ['./productreview.component.scss'],
 })
 export class ProductreviewComponent implements OnInit {
   captchaForm: FormGroup;
@@ -17,24 +16,24 @@ export class ProductreviewComponent implements OnInit {
     public deckService: DeckService,
     public translate: TranslateService,
     private formBuilder: FormBuilder
-
-  ) { }
+  ) {}
 
   async ngAfterContentInit() {
-    this.deckService.userCanBack()
-    this.deckService.userCanContinue()
+    this.deckService.userCanBack();
+    this.deckService.userCanContinue();
   }
   async ngOnInit() {
     // this.deckService.userCanBack()
     // this.deckService.userCannotContinue()
-    this.deckService.setCaptchaNotCleared()
+    this.deckService.setCaptchaNotCleared();
     this.isPermittedLocation = await this.deckService.isPermittedLocation();
     this.captchaForm = this.formBuilder.group({
-      recaptcha: ['', Validators.required]
+      recaptcha: ['', Validators.required],
     });
   }
- 
-
-
-
+  handleSuccess(event) {
+    // add verification step
+    this.deckService.setCaptchaCleared()
+  }
+  
 }
