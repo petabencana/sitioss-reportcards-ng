de6afdba01b429e2e05ac7a205417a0242576360
sanjaypinna <sanjaypinna265@gmail.com>
2024-02-12 19:34:12 +0530

    add units

--- src/app/components/donation-cards/donation-list.ts
+++ src/app/components/donation-cards/donation-list.ts
@@ -8,6 +8,7 @@ export const donationList = [
     quantity: 4,
     status: '0% Met',
     donate: 0,
+    units: 'Liters',
   },
   {
     title: 'Rice',
@@ -17,14 +18,18 @@ export const donationList = [
     quantity: 3,
     status: '0% Met',
     donate: 0,
+    units: 'kgs',
   },
   {
     title: 'Ready to Eat Meals',
-    description: 'Super food',
+    description:
+      'Super food ver good food Super food ver good foodSuper food ver good food',
     category: 'FOOD/WATER',
     img: '../../../assets/decks/logistics/products/ReadytoEatMeals.png',
     quantity: 2,
     status: '0% Met',
     donate: 0,
+    units: 'packs',
   },
+  
 ];
