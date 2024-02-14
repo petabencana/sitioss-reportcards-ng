d898bfb837182f1a76df810a59059e7b035495fa
sanjaypinna <sanjaypinna265@gmail.com>
2024-02-14 18:57:27 +0530

    add donation service for translations

---
+++ src/app/components/donation-cards/donation.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TranslationService {
+
+  constructor(private http: HttpClient) {}
+
+  getTranslations(locale: string): Observable<any> {
+    return this.http.get<any>(`assets/locales/${locale}.json`).pipe(
+      map((data) => data.card.productsList)
+    );
+  }
+  
+  getDonationTranslations(locale: string): Observable<any> {
+    return this.http.get<any>(`assets/locales/${locale}.json`).pipe(
+      map((data) => data.card.productsList)
+    );
+  }
+
+  mapDonationsWithNeedId(
+    donations: any[],
+    productsList: any[],
+    locale: string
+  ): any[] {
+    const translationKey = 'productsList';
+    if (!productsList) {
+      console.error('Products list not found');
+      return donations;
+    }
+  
+    const products = productsList[translationKey];
+    if (!products) {
+      console.error(`Translations not found for key: ${translationKey}`);
+      return donations;
+    }
+  
+    // Map donations with need_id
+    return donations.map((donation) => {
+      const product = products.find((p) => p.need_id === donation.need_id);
+      if (product) {
+        return { ...donation, ...product };
+      } else {
+        return donation;
+      }
+    });
+  }
+
+}
