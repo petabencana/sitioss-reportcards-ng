a87bd9cd43954e494ca76093bdeecd255168b1e6
sanjaypinna <sanjaypinna265@gmail.com>
2024-02-22 15:08:35 +0530

    add need_id, fetch data from api and add loading state

--- src/app/components/donation-cards/donation-card.component.ts
+++ src/app/components/donation-cards/donation-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DeckService } from '../../services/cards/deck.service';
 import { TranslationService } from './donation.service';
+import { environment as env } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { donationList } from './donation-list';
 
 @Component({
@@ -10,31 +12,35 @@ import { donationList } from './donation-list';
 })
 export class DonationCardComponent implements OnInit {
   quantity: number = 0;
-  cards: any[];
+  giverCards: any[] = [];
   translatedData: any[]; // Store translated data
-
+  loadingData: boolean = true;
   constructor(
     private translationService: TranslationService,
-    public deckService: DeckService
+    public deckService: DeckService,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
     // Check if translated data is already available in the state
     this.translatedData = this.deckService.getTranslatedData();
+    this.giverCards = this.deckService.getGiverCards() || [];
 
-    // If translated data is not available, fetch it from the translation service
-    if (!this.translatedData) {
-      this.translationService.getDonationTranslations('en').subscribe(donationTranslations => {
-        if (donationTranslations) {
-          this.translatedData = donationTranslations; // Store translated data
-          this.deckService.setTranslatedData(donationTranslations); // Store translated data in the state
-          this.loadCards(); // Load cards using translated data
-        } else {
-          console.error('Donation translations not found');
-        }
-      });
+    // If translated data is not available or giverCards is empty, fetch it
+    if (!this.translatedData || this.giverCards.length === 0) {
+      this.translationService
+        .getDonationTranslations('en')
+        .subscribe((donationTranslations) => {
+          if (donationTranslations) {
+            this.translatedData = donationTranslations; // Store translated data
+            this.deckService.setTranslatedData(donationTranslations); // Store translated data in the state
+            this.loadCards(); // Load cards using translated data
+          } else {
+            console.error('Donation translations not found');
+          }
+        });
     } else {
-      // If translated data is already available, load cards directly
+      // If translated data and giverCards are available, load cards directly
       this.loadCards();
     }
 
@@ -43,20 +49,59 @@ export class DonationCardComponent implements OnInit {
   }
 
   private loadCards() {
-    // Use translated data to map donation list
-    this.cards = donationList.map(donation => {
-      const product = this.translatedData.find(p => p.item_id === donation.item_id);
-      return product ? { ...donation, title: product.title } : donation;
-    });
+    this.loadingData = true; // Set loading state to true    // Fetch cards
+    this.http
+      .get<any[]>(
+        `${env.data_server}needs/need?requestId=${this.deckService.requestId}`
+      )
+      .subscribe((apiDonationData) => {
+        if (apiDonationData) {
+          this.giverCards = apiDonationData.map((donation) => {
+            const product = this.translatedData.find(
+              (p) => p.item_id === donation.item_id
+            );
+            const title = product ? product.title : donation.title;
+            return {
+              title: title,
+              description: donation.description || '',
+              category: product.category,
+              img: product.img,
+              quantity: +donation.quantity_requested,
+              status:
+                donation.quantity_satisfied !== null
+                  ? `${(
+                      (donation.quantity_satisfied /
+                        donation.quantity_requested) *
+                      100
+                    ).toFixed(0)}% Met`
+                  : '0% Met',
+              donate:
+                donation.quantity_satisfied !== null
+                  ? +donation.quantity_satisfied
+                  : 0,
+              units: product ? product.units : '', // Add units from product if available
+              item_id: donation.item_id,
+              need_id: donation.id,
+            };
+          });
+          this.deckService.setTranslatedData(this.giverCards);
 
-    // Fetch selected products from the DeckService for each card
-    this.cards.forEach(card => {
-      const selectedProduct = this.deckService.getSelectedProducts(card.title);
-      if (selectedProduct) {
-        card.donate = selectedProduct.donate;
-      }
-    });
+          // Fetch selected products from the DeckService for each card
+          this.giverCards.forEach((card) => {
+            const selectedProduct = this.deckService.getSelectedProducts(
+              card.title
+            );
+            if (selectedProduct) {
+              card.donate = selectedProduct.donate;
+            }
+          });
+        } else {
+          console.error('API donation data not found');
+        }
+        this.loadingData = false; // Set loading state to false
+      });
   }
+
   private recordQuantityChange(
     title: string,
     quantity: number,
@@ -65,6 +110,7 @@ export class DonationCardComponent implements OnInit {
     img: string,
     units: string,
     item_id: string,
+    need_id: number,
     donate: number
   ) {
     this.deckService.setSelectedProducts(
@@ -75,6 +121,7 @@ export class DonationCardComponent implements OnInit {
       img,
       units,
       item_id,
+      need_id,
       donate
     );
   }
@@ -90,6 +137,7 @@ export class DonationCardComponent implements OnInit {
         card.img,
         card.units,
         card.item_id,
+        card.need_id,
         card.donate
       );
     }
@@ -108,6 +156,7 @@ export class DonationCardComponent implements OnInit {
           card.img,
           card.units,
           card.item_id,
+          card.need_id,
           card.donate
         );
       } else {
@@ -120,6 +169,7 @@ export class DonationCardComponent implements OnInit {
           card.img,
           card.units,
           card.item_id,
+          card.need_id,
           card.donate
         );
       }
@@ -135,9 +185,9 @@ export class DonationCardComponent implements OnInit {
   }
 
   check() {
-    console.log(this.deckService.selectedProducts);
-    console.log(this.cards);
-    
+    // console.log(this.deckService.selectedProducts);
+    console.log(this.giverCards);
+    // console.log(this.translatedData);
   }
 
   openDescriptionModal(card: any): void {
