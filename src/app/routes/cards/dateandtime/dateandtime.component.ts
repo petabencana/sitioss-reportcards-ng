26140ac0ff88d1fad7fcfceabc58f4fd70f993af
sanjaypinna <sanjaypinna265@gmail.com>
2024-02-23 12:32:07 +0530

    fix validations and remove logs

--- src/app/routes/cards/dateandtime/dateandtime.component.ts
+++ src/app/routes/cards/dateandtime/dateandtime.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeckService } from '../../../services/cards/deck.service';
 import flatpickr from 'flatpickr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dateandtime',
@@ -11,15 +12,34 @@ export class DateandtimeComponent implements OnInit {
   selectedDate: string;
   selectedTime: string;
 
-  constructor(public deckService: DeckService) {}
+  constructor(
+    public deckService: DeckService,
+    public translate: TranslateService
+  ) {}
 
   ngOnInit() {
     this.deckService.userCanBack();
-    this.deckService.userCanContinue();
     this.selectedDate = this.deckService.getDonationdate() || '';
     this.selectedTime = this.deckService.getDonationtime() || '';
     this.openCalendar();
     this.openTimePicker();
+
+    if (
+      this.deckService.donationDate !== undefined &&
+      this.deckService.donationTime !== undefined
+    ) {
+      this.deckService.userCanContinue();
+    } else {
+      this.deckService.userCannotContinue();
+    }
+  }
+
+  get dateandtimeHeader(): string {
+    return this.translate.instant('card.giverLabels.dateandtimeHeader');
+  }
+
+  get dateandtimeDescription(): string {
+    return this.translate.instant('card.giverLabels.dateandtimeDescription');
   }
 
   openCalendar() {
@@ -27,10 +47,20 @@ export class DateandtimeComponent implements OnInit {
       inline: true,
       defaultDate: this.selectedDate, // Prefill calendar with selected date
       minDate: new Date().toISOString().split('T')[0],
-      maxDate: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000).toISOString().split('T')[0],
+      maxDate: new Date(new Date().getTime() + 30 * 24 * 60 * 60 * 1000)
+        .toISOString()
+        .split('T')[0],
       onChange: (selectedDates, dateStr, instance) => {
         this.selectedDate = dateStr;
         this.deckService.setDonationdate(this.selectedDate);
+        if (
+          this.deckService.donationDate !== undefined &&
+          this.deckService.donationTime !== undefined
+        ) {
+          this.deckService.userCanContinue();
+        } else {
+          this.deckService.userCannotContinue();
+        }
       },
     });
   }
@@ -45,14 +75,17 @@ export class DateandtimeComponent implements OnInit {
       onChange: (selectedDates, dateStr, instance) => {
         this.selectedTime = dateStr;
         this.deckService.setDonationtime(this.selectedTime);
+        if (
+          this.deckService.donationDate !== undefined &&
+          this.deckService.donationTime !== undefined
+        ) {
+          this.deckService.userCanContinue();
+        } else {
+          this.deckService.userCannotContinue();
+        }
       },
     });
   }
 
-  check() {
-    console.log(
-      this.deckService.getDonationdate(),
-      this.deckService.getDonationtime()
-    );
-  }
+ 
 }
